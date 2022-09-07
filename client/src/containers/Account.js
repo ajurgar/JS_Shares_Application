9e58f093dc9707c57615a3162a22ae693abeec16
Harpreet Dhanda <harpreet_h@hotmail.co.uk>
2022-09-07 12:56:23 +0100

    Money Falling - audio added

--- client/src/containers/Account.js
+++ client/src/containers/Account.js
@@ -1,17 +1,57 @@
 import React from "react";
+import './Account.css';
+import ReactAudioPlayer from 'react-audio-player';
+import { ReactDOM } from "react";
+
+
+
+const mp3 = require( "./herecomemoney.mp3")
 
 const Account = () => {
 
     return (
+        <div className="">
+        <div className="loader">
+
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+
         <div className="account">
             <section className="details">
+ 
                 <h2>
                     Personal Details
                 </h2>
                 <p>Name: Ben Robert</p>
                 <p>Email: BenRoberts@codeclan.com</p>
                 <p>Password: ****** <button> Change Password </button></p>
-                <p className="upgradebutton">Membership: Free <button className="upgradebutton"><a href="https://www.alphavantage.co/support/#"> Upgrade Membership</a></button></p>
+                <p>Membership: Free <button className="upgradebutton"><a href="https://www.alphavantage.co/support/#"> Upgrade Membership</a></button></p>
             </section>
             <section className="details">
                 <h2>
@@ -20,6 +60,11 @@ const Account = () => {
                 <p>Bank Name: Starling</p>
                 <p>Bank Account Number: 23-41-**-** <button> Change Account Number </button></p>
                 <p>Sort-Code: 80-75-60<button> Change Sort-Code </button></p>
+               <p><ReactAudioPlayer
+  src={mp3}
+  autoplay={false}
+  controls
+/></p>
             </section>
             <section className="details">
                 <h2>Subscription</h2>
@@ -28,6 +73,8 @@ const Account = () => {
                 <p>Post <input type="checkbox" /></p>
                 <p><button> Unsubscribe</button></p>
             </section>
+        </div>
+        </div>
         </div>
     )
 }
